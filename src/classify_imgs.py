f36a809c3d298ad7bdc4e2015d7eef9ffefbe9a1
xxsmario <xxsmario@proton.me>
2023-06-05 14:46:33 +0000

    Fixed some strings

--- src/classify_imgs.py
+++ src/classify_imgs.py
@@ -17,4 +17,5 @@ def parse_args():
 	parser = argparse.ArgumentParser(description='STEP 06/06 - Classify a list of images' + \
 		' using a trained model.')
 	parser.add_argument("-i", "--images", nargs='+', help='<Required> List of input images' + \
-		' that will be classified.', re
+		' that will be classified.', required=True)
+	parser.add_argu
