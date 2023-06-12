a8fd4d43699f92e001c668c31043d1671ef26693
xxsmario <xxsmario@proton.me>
2023-06-12 14:58:02 +0000

    Update code.md

--- src/classify_imgs.py
+++ src/classify_imgs.py
@@ -21,4 +21,4 @@ def parse_args():
 	parser.add_argument("-m", "--model-dir", help='<Required> Input directory with' + \
 		' the trained model and the tensorboard logs.', required=True)
 	parser.add_argument("-o", "--output-dir", help='<Required> The output directory that will ' + \
-		' that
+		' that will have the classification
