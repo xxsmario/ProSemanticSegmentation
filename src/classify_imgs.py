189b49a20fddb1572561a08c859272e594f5e6d6
xxsmario <xxsmario@proton.me>
2023-06-29 19:30:02 +0000

    pagina de confirmacion

--- src/classify_imgs.py
+++ src/classify_imgs.py
@@ -39,4 +39,6 @@ def exec(images, model_dir, output_dir, memory_percentage = 40):
 	chips_info_path = dl_utils.new_filepath('chips_info.dat', directory=model_dir)
 	chips_info = dl_utils.load_object(chips_info_path)
 
-	for in_image in imag
+	for in_image in images:
+
+		in_image_ds = gdal.Open(
