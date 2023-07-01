38058485949431d0fc39be3e75b8799cb2155906
xxsmario <xxsmario@proton.me>
2023-07-01 14:59:50 +0000

    Initial styling

--- src/classify_imgs.py
+++ src/classify_imgs.py
@@ -42,4 +42,4 @@ def exec(images, model_dir, output_dir, memory_percentage = 40):
 	for in_image in images:
 
 		in_image_ds = gdal.Open(in_image)
-		out_image = dl_utils.n
+		out_image = dl_utils.new_filepath(in_image, 
