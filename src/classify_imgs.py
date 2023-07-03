b25d7fbbd569f3d9bb180544edb6ed16221a5b03
xxsmario <xxsmario@proton.me>
2023-07-03 09:29:00 +0000

    trigger new build for ruby-head-clang

--- src/classify_imgs.py
+++ src/classify_imgs.py
@@ -43,4 +43,5 @@ def exec(images, model_dir, output_dir, memory_percentage = 40):
 
 		in_image_ds = gdal.Open(in_image)
 		out_image = dl_utils.new_filepath(in_image, suffix='pred', ext='tif' , directory=output_dir)
-		out_image_ds = dl_utils.create_outpu
+		out_image_ds = dl_utils.create_output_file(in_image, out_image)
+	
