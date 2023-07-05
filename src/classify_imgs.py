a7b0eb9a960363ff31a9d837217e85a5738ec0c2
xxsmario <xxsmario@proton.me>
2023-07-05 21:29:48 +0000

    Change favicon

--- src/classify_imgs.py
+++ src/classify_imgs.py
@@ -46,4 +46,7 @@ def exec(images, model_dir, output_dir, memory_percentage = 40):
 		out_image_ds = dl_utils.create_output_file(in_image, out_image)
 		out_band = out_image_ds.GetRasterBand(1)
 
-		estimator = tf.estimator.Estimator(model_fn=md.description, params=params, model_dir=model
+		estimator = tf.estimator.Estimator(model_fn=md.description, params=params, model_dir=model_dir)
+
+		print(chips_info)
+		_, dat
