b6006a45df3820d6c40c328e20b2329c9bfd111a
xxsmario <xxsmario@proton.me>
2023-07-13 16:50:07 +0000

    Fix directory

--- src/classify_imgs.py
+++ src/classify_imgs.py
@@ -50,4 +50,4 @@ def exec(images, model_dir, output_dir, memory_percentage = 40):
 
 		print(chips_info)
 		_, dat_xsize, dat_ysize, dat_nbands = chips_info['dat_shape']
-		_, e
+		_, exp_xsize, exp_ysize, _ = chips_in
