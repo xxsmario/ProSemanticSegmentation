24d690f9961dbfd4e2e3e156deee840df54eee0b
xxsmario <xxsmario@proton.me>
2023-08-09 09:46:08 +0000

    Win32: make FILETIME conversion functions public

--- src/classify_imgs.py
+++ src/classify_imgs.py
@@ -84,4 +84,4 @@ def exec(images, model_dir, output_dir, memory_percentage = 40):
 
 				tensors_to_log = {}
 
-				print("Classifying image " + in_image + ": progress " + str(flo
+				print("Classifying image " + in_image + ": progress " + str(float(i)/len(input_positions)*100)
