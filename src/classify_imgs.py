953be1ba67b107aad358750a27c39431ee936b84
xxsmario <xxsmario@proton.me>
2023-08-11 09:01:04 +0000

    Fixing error message

--- src/classify_imgs.py
+++ src/classify_imgs.py
@@ -86,4 +86,6 @@ def exec(images, model_dir, output_dir, memory_percentage = 40):
 
 				print("Classifying image " + in_image + ": progress " + str(float(i)/len(input_positions)*100) + "%" )
 				predict_input_fn = tf.estimator.inputs.numpy_input_fn(x={"data": input_data}, batch_size=params['batch_size'], shuffle=False)
-				predict_results = estimator.predict(input_fn=predic
+				predict_results = estimator.predict(input_fn=predict_input_fn)
+
+				print("Writing
