56b5311cd23eca7642ead9212570c33699b0055b
xxsmario <xxsmario@proton.me>
2023-08-24 10:44:31 +0000

    moar fixes

--- src/classify_imgs.py
+++ src/classify_imgs.py
@@ -111,4 +111,4 @@ def exec(images, model_dir, output_dir, memory_percentage = 40):
 						out_ylen = in_image_ds.RasterYSize - out_y0
 						out_data = out_data[0:out_ylen,:]
 						print('out_y0', out_data.shape)
-						print(ou
+						print(out_y0, in_image_ds.RasterY
