47b547fce1cf58a7fd06a547c25abd78f1c4af95
xxsmario <xxsmario@proton.me>
2023-08-31 20:34:03 +0000

    add a new note

--- src/classify_imgs.py
+++ src/classify_imgs.py
@@ -131,4 +131,8 @@ if __name__ == "__main__":
 	model_dir = args.model_dir
 	images = args.images
 	output_dir = args.output_dir
-	memory_percentage = args.memory_percent
+	memory_percentage = args.memory_percentage
+
+	start_time = time.time()
+
+	exec(imag
