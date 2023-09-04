6ba4f2c12139e18d89c51acf7e7e7dedf4baf488
xxsmario <xxsmario@proton.me>
2023-09-04 06:50:33 +0000

    Update workspace.xml

--- src/dl_utils.py
+++ src/dl_utils.py
@@ -94,3 +94,38 @@ def chips_info(img_path, nodata_value, chip_size, pad_size, offset_list=[(0,0)],
 		
 		for input_position in input_positions:
 			chip_data, _ = get_predict_data(input_img_ds, input_position, pad_size)
+
+			chip_data, chip_expect = split_data(chip_data, pad_size)
+			xsize, ysize, _ = chip_expect.shape
+
+			if (chip_size == xsize and chip_size == ysize) and (not discard_nodata or float(np.max(chip_expect)) != float(nodata_value)):
+				total_nchips += 1
+	
+	if rotate:
+		total_nchips *= 4
+	if flip:
+		total_nchips *= 2
+		
+	sample_input_position = input_positions[0]
+	chip_data, _ = get_predict_data(input_img_ds, sample_input_position, pad_size)
+
+	chip_data, chip_expect = split_data(chip_data, pad_size)
+
+	dat_width, dat_height, dat_nbands = chip_data.shape
+	exp_width, exp_height, exp_nbands = chip_expect.shape
+
+	return {
+		'dat_shape': (total_nchips, dat_width, dat_height, dat_nbands),
+		'exp_shape': (total_nchips, exp_width, exp_height, exp_nbands),
+		'dat_dtype': chip_data.dtype,
+		'exp_dtype': chip_expect.dtype,
+		'n_chips': total_nchips
+	}
+
+def generate_chips(img_path, dat_ndarray, exp_ndarray, nodata_value, chip_size, pad_size, \
+							offset_list=[(0,0)], rotate=False, flip=False, discard_nodata=True, simulate=False):
+	
+	index = 0
+	input_img_ds = gdal.Open(img_path)
+
+	for x_offset_percent, y_offset_percent in offset_list:
