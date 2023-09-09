9ed62633322caa6901d2e0972b7de8d2d89cf77e
xxsmario <xxsmario@proton.me>
2023-09-09 10:00:10 +0000

    Updated project setup.

--- src/evaluate_model.py
+++ src/evaluate_model.py
@@ -43,3 +43,29 @@ def do_evaluation(estimator, input_data, input_expected, category, params):
 	accuracy = accuracy_score(ref_flat_arr, pred_flat_arr)
 	print(category + ' accurancy:', accuracy)
 	print('\n--------------------------------------------------')
+	print('------------- '+category+' METRICS -------------')
+	print('--------------------------------------------------')
+	print(classification_report(ref_flat_arr, pred_flat_arr))
+
+	print('\n--------------------------------------------------')
+	print('------------- CONFUSION MATRIX -------------')
+	print('--------------------------------------------------')
+	print(confusion_matrix(ref_flat_arr, pred_flat_arr))
+
+def parse_args():
+	parser = argparse.ArgumentParser(description='STEP 05/06 - Evaluate a ' + \
+		' trained model.')
+	parser.add_argument("-m", "--model-dir", help='<Required> Input directory with' + \
+		' the trained model and the tensorboard logs.', required=True) 
+	parser.add_argument("-s", "--eval-size", help='Percentage size of chips that will be' + \
+		' used in the evaluation [DEFAULT=Value defined by train_model.py]', type=float, default=0)
+	parser.add_argument("-i", "--chips-dir", help='Input directory of chips' + \
+		' that will be used by evaluation process [DEFAULT=Value defined by train_model.py]', \
+		 default=None)
+
+	return parser.parse_args()
+
+def exec(model_dir, chips_dir, eval_size):
+	tf.logging.set_verbosity(tf.logging.INFO)
+
+	start_time = time.time()
