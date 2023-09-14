da1a7b7bb250692877621533c5bd97ac5cf7f1b0
xxsmario <xxsmario@proton.me>
2023-09-14 17:15:24 +0000

    misc: small fix or general refactoring i did not bother commenting

--- src/generate_chips.py
+++ src/generate_chips.py
@@ -11,4 +11,5 @@ import numpy as np
 def parse_args():
 	parser = argparse.ArgumentParser(description='STEP 03/06 - ' + \
 		' Generate a several chips (i.e. a set of pixels with regular squared size) ' + \
-		' considerering the input image. The last band will be
+		' considerering the input image. The last band will be used' + \
+		' as expected outp
