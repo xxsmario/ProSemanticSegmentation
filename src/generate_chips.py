b675ab578950d88d8811de149207128cc4b66849
xxsmario <xxsmario@proton.me>
2023-10-18 06:28:26 +0000

    Add helper methods

--- src/generate_chips.py
+++ src/generate_chips.py
@@ -26,4 +26,4 @@ def parse_args():
 		 ' A padding size of 93px and a chip size of 100px will result in a input chip of 286px. [DEFAULT=93]', type=int, default=93)
 	parser.add_argument("-f", "--offset", help='As a data augmentation option, ' + \
 		' offset argument will be used to produce chips with a percentage of overlap.' + \
-	
+		' An offset 0,50 will generate 
