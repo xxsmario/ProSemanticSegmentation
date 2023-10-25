f97eabd803fe82e09017929af81e2a76f78eb5bf
xxsmario <xxsmario@proton.me>
2023-10-25 14:45:40 +0000

    build.

--- src/generate_chips.py
+++ src/generate_chips.py
@@ -30,4 +30,4 @@ def parse_args():
 	parser.add_argument("-r", "--rotate", help='As a data augmentation option, ' + \
 		' rotate argument will rotate all the chips at angles 90, 180 and 270 degrees. [DEFAULT=false]', action='store_true')
 	parser.add_argument("-u", "--shuffle", help='Shuffle generated chips. ' + \
-		' If the generated chips is only for test propose, you should set false here. 
+		' If the generated chips is only for test propose, you should set false here. [DEFAULT=true]', action='store_t
