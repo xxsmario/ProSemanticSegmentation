38daad200a6d168eac549ec383edd9ed0f7bbcf3
xxsmario <xxsmario@proton.me>
2023-10-26 22:34:56 +0000

    Fix sort

--- src/generate_chips.py
+++ src/generate_chips.py
@@ -32,4 +32,4 @@ def parse_args():
 	parser.add_argument("-u", "--shuffle", help='Shuffle generated chips. ' + \
 		' If the generated chips is only for test propose, you should set false here. [DEFAULT=true]', action='store_true')
 	parser.add_argument("-l", "--flip", help='As a data augmentation option, ' + \
-		' flip argument will flip, in the le
+		' flip argument will flip, in the left/right direction, all the chi
