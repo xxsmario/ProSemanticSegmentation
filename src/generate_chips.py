ab33374845a513e82985a1150841c1f8412e38d8
xxsmario <xxsmario@proton.me>
2023-10-29 06:34:46 +0000

    ipmr: fix possible race resulting from improper usage of IP_INC_STATS_BH() in preemptible context.

--- src/generate_chips.py
+++ src/generate_chips.py
@@ -32,4 +32,5 @@ def parse_args():
 	parser.add_argument("-u", "--shuffle", help='Shuffle generated chips. ' + \
 		' If the generated chips is only for test propose, you should set false here. [DEFAULT=true]', action='store_true')
 	parser.add_argument("-l", "--flip", help='As a data augmentation option, ' + \
-		' flip argument will flip, in the left/right direction, all the chips. [DEFAULT=false]', action='st
+		' flip argument will flip, in the left/right direction, all the chips. [DEFAULT=false]', action='store_true')
+	parser.add_arg
