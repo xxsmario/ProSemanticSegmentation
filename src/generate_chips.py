08a4cbead17e7611fc858e4aa6913193b40086e9
xxsmario <xxsmario@proton.me>
2023-11-05 09:21:23 +0000

    new projects

--- src/generate_chips.py
+++ src/generate_chips.py
@@ -34,4 +34,6 @@ def parse_args():
 	parser.add_argument("-l", "--flip", help='As a data augmentation option, ' + \
 		' flip argument will flip, in the left/right direction, all the chips. [DEFAULT=false]', action='store_true')
 	parser.add_argument("-d", "--discard-nodata", help='Chips with nodata values will be discard by' + \
-		' chip generation process. You shouldn\'t considerer put true here. [DEFAULT=false]'
+		' chip generation process. You shouldn\'t considerer put true here. [DEFAULT=false]', action='store_true')
+	
+	ret
