24f9ea8baf6ebcc52010377e8d4964cbfae07605
xxsmario <xxsmario@proton.me>
2023-12-21 09:06:42 +0000

    Run scan-local.sh on swift:...

--- src/run_toy_forest.sh
+++ src/run_toy_forest.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-echo "00) ------------------ Downloading and extracting forest_toy.zip -----
+echo "00) ------------------ Downloading and extracting forest_toy.zip ------------------"
+rm -R fore
