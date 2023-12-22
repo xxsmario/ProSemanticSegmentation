542a2d02203cd25d0510e996554f7463f633788c
xxsmario <xxsmario@proton.me>
2023-12-22 22:38:35 +0000

    Ignore .idea folder

--- src/run_toy_forest.sh
+++ src/run_toy_forest.sh
@@ -2,4 +2,4 @@
 
 echo "00) ------------------ Downloading and extracting forest_toy.zip ------------------"
 rm -R forest_toy forest_toy.zip
-wget https://storage.googleapis.com/nextgenmap-d
+wget https://storage.googleapis.com/nextgenmap-dataset/dl-semantic-segmentatio
