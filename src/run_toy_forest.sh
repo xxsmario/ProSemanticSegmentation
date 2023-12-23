07685544a56b0dba6b0d8183d8903160f0f95a8d
xxsmario <xxsmario@proton.me>
2023-12-23 19:45:33 +0000

    Refactoring (Rubocop corrections).

--- src/run_toy_forest.sh
+++ src/run_toy_forest.sh
@@ -2,4 +2,5 @@
 
 echo "00) ------------------ Downloading and extracting forest_toy.zip ------------------"
 rm -R forest_toy forest_toy.zip
-wget https://storage.googleapis.com/nextgenmap-dataset/dl-semantic-segmentatio
+wget https://storage.googleapis.com/nextgenmap-dataset/dl-semantic-segmentation/forest_toy.zip
+unzip forest_t
