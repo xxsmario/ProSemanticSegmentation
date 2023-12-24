daabfc6570f6369610c535ef50eab9528bf2226f
xxsmario <xxsmario@proton.me>
2023-12-24 20:45:52 +0000

    Updates Podfile.lock

--- src/run_toy_forest.sh
+++ src/run_toy_forest.sh
@@ -5,4 +5,4 @@ rm -R forest_toy forest_toy.zip
 wget https://storage.googleapis.com/nextgenmap-dataset/dl-semantic-segmentation/forest_toy.zip
 unzip forest_toy.zip
 
-echo "01) -----------------
+echo "01) ------------------ Running standardize_imgs.
