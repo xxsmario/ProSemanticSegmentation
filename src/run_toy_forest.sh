f9cf28d663f3436cebdbd3023632fbde7622e18c
xxsmario <xxsmario@proton.me>
2023-12-24 20:51:35 +0000

    new features

--- src/run_toy_forest.sh
+++ src/run_toy_forest.sh
@@ -5,4 +5,5 @@ rm -R forest_toy forest_toy.zip
 wget https://storage.googleapis.com/nextgenmap-dataset/dl-semantic-segmentation/forest_toy.zip
 unzip forest_toy.zip
 
-echo "01) ------------------ Running standardize_imgs.
+echo "01) ------------------ Running standardize_imgs.py ------------------"
+./st
