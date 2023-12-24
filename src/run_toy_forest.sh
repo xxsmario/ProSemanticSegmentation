610d748381dcc3c2f232ba49153309fdae30f337
xxsmario <xxsmario@proton.me>
2023-12-24 18:34:14 +0000

    chore(package): update istanbul to version 0.4.2

--- src/run_toy_forest.sh
+++ src/run_toy_forest.sh
@@ -3,4 +3,6 @@
 echo "00) ------------------ Downloading and extracting forest_toy.zip ------------------"
 rm -R forest_toy forest_toy.zip
 wget https://storage.googleapis.com/nextgenmap-dataset/dl-semantic-segmentation/forest_toy.zip
-unzip forest_t
+unzip forest_toy.zip
+
+echo "01) -----------------
