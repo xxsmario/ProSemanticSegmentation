95064ea373b4ce3d1f2fff13b2a8ce12600cd98a
xxsmario <xxsmario@proton.me>
2023-12-26 20:24:57 +0000

    better readability

--- src/run_toy_forest.sh
+++ src/run_toy_forest.sh
@@ -6,4 +6,6 @@ wget https://storage.googleapis.com/nextgenmap-dataset/dl-semantic-segmentation/
 unzip forest_toy.zip
 
 echo "01) ------------------ Running standardize_imgs.py ------------------"
-./standardize_imgs.py -n 0 -b 1 2 3 4 -i forest_toy/raw_data/mosaic_201709.tif forest_toy/raw_data/mosaic_201801.tif -o forest_toy/stand_d
+./standardize_imgs.py -n 0 -b 1 2 3 4 -i forest_toy/raw_data/mosaic_201709.tif forest_toy/raw_data/mosaic_201801.tif -o forest_toy/stand_data
+
+echo "02) ------------------ R
