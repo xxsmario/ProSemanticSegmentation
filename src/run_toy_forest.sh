6d48656f23b09ce239ee96b329bb543c1397313d
xxsmario <xxsmario@proton.me>
2023-12-27 12:52:27 +0000

    Error handling.

--- src/run_toy_forest.sh
+++ src/run_toy_forest.sh
@@ -8,4 +8,4 @@ unzip forest_toy.zip
 echo "01) ------------------ Running standardize_imgs.py ------------------"
 ./standardize_imgs.py -n 0 -b 1 2 3 4 -i forest_toy/raw_data/mosaic_201709.tif forest_toy/raw_data/mosaic_201801.tif -o forest_toy/stand_data
 
-echo "02) ------------------ R
+echo "02) ------------------ Running stack_imgs.py --------
