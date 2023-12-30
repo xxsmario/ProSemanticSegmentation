c65eb6da12e93ef7ad907b0d49bd15bc0a6713a0
xxsmario <xxsmario@proton.me>
2023-12-30 14:06:23 +0000

    tickets

--- src/run_toy_forest.sh
+++ src/run_toy_forest.sh
@@ -9,4 +9,4 @@ echo "01) ------------------ Running standardize_imgs.py ------------------"
 ./standardize_imgs.py -n 0 -b 1 2 3 4 -i forest_toy/raw_data/mosaic_201709.tif forest_toy/raw_data/mosaic_201801.tif -o forest_toy/stand_data
 
 echo "02) ------------------ Running stack_imgs.py ------------------"
-./stack_imgs.py -i forest_toy/stand_data/mosaic_201709_stand.tif -r forest_toy/raw_data/forest_201709.tif -o forest_toy/stand_data/fore
+./stack_imgs.py -i forest_toy/stand_data/mosaic_201709_stand.tif -r forest_toy/raw_data/forest_201709.tif -o forest_toy/stand_data/forest_201709_model_input.v
