25f488f3bd36edbafb7bf0818bb9ae4ae1e6c066
xxsmario <xxsmario@proton.me>
2024-01-03 16:58:35 +0000

    Solving conflicts

--- src/run_toy_forest.sh
+++ src/run_toy_forest.sh
@@ -15,4 +15,6 @@ echo "03) ------------------ Running generate_chips.py ------------------"
 ./generate_chips.py -f 0,0 -r -l -u -i forest_toy/stand_data/forest_201709_model_input.vrt -o forest_toy/chips
 
 echo "04) ------------------ Running train_model.py ------------------"
-./train_model.py -e 20 -i forest_toy/chips -o forest_to
+./train_model.py -e 20 -i forest_toy/chips -o forest_toy/model/
+
+echo "05) ----------
