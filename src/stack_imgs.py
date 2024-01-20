d5f05f068c63a4ea6bdc206eadc8a9883a4f97a0
xxsmario <xxsmario@proton.me>
2024-01-20 12:58:01 +0000

    Added grid

--- src/stack_imgs.py
+++ src/stack_imgs.py
@@ -15,4 +15,4 @@ def parse_args():
 	parser.add_argument("-i", "--images", nargs='+', help='<Required> List of input images.', required=True)
 	parser.add_argument("-b", "--bands", nargs='+', type=int, help='The bands that should be stacked. [DEFAULT=All]', default=None)
 	parser.add_argument("-r", "--reference", help=' Image with reference data, that should have only these pixel values:' + \
-		' 0=without 
+		' 0=without information, 1=object of
